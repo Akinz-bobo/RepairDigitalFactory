93d0410eb61c4f34f7f86d3ebc2a017809d7b9ab
Akinz-bobo <adeyemoak1997@gmail.com>
2023-09-06 18:23:29 +0100

    search component changed

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -9,11 +9,10 @@ const Search = () => {
 
   return createPortal(
     <section
-      className={`${
-        ActiveSearch == false
+      className={`${ActiveSearch == false
           ? Styles["search--container"]
           : Styles["search--container-show"]
-      }
+        }
     flex items-center justify-center bg-[#000000e6] w-full h-full fixed top-0 left-0 z-50`}
     >
       <button
@@ -36,8 +35,8 @@ const Search = () => {
         <label htmlFor="search--container"></label>
         <input
           className="w-full h-full text-white bg-transparent border-b-4 border-white focus:border-red-500"
-          dir="rtl"
-          placeholder="خدمات مورد نظر را جستجو کنید!!"
+          dir="ltr"
+          placeholder="Search for your desired service!"
           type="text"
           id="search"
         />
